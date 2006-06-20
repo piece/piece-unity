c91f5e77fe944192aad22168cef9366c73e8cb02
KUBO Atsuhiro <kubo@iteman.jp>
2006-06-20 01:57:50 +0000

    - Added.

---
+++ tests/Piece/Unity/Plugin/PHPRendererBuiltinElements.php
@@ -0,0 +1,8 @@
+<?php
+if (isset($__request) && is_a($__request, 'Piece_Unity_Request')
+    && isset($__session) && is_a($__session, 'Piece_Unity_Session_Common')
+    ) {
+    print 'OK';
+} else {
+    print 'NG';
+}
