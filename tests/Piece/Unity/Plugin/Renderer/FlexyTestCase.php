4966622caaac63cfb458238f2777e9ea7786de71
KUBO Atsuhiro <kubo@iteman.jp>
2006-07-01 04:22:06 +0000

    - Added.

---
+++ tests/Piece/Unity/Plugin/Renderer/FlexyTestCase.php
@@ -0,0 +1,132 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP versions 4 and 5
+ *
+ * Copyright (c) 2006, KUBO Atsuhiro <iteman2002@yahoo.co.jp>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Piece_Unity
+ * @author     KUBO Atsuhiro <iteman2002@yahoo.co.jp>
+ * @copyright  2006 KUBO Atsuhiro <iteman2002@yahoo.co.jp>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    SVN: $Id$
+ * @link       http://iteman.typepad.jp/piece/
+ * @see        Piece_Unity_Plugin_Renderer_Flexy
+ * @since      File available since Release 0.2.0
+ */
+
+require_once 'Piece/Unity/Plugin/Renderer/Flexy.php';
+require_once dirname(__FILE__) . '/CompatibilityTest.php';
+
+// {{{ Piece_Unity_Plugin_Renderer_FlexyTestCase
+
+/**
+ * TestCase for Piece_Unity_Plugin_Renderer_Flexy
+ *
+ * @package    Piece_Unity
+ * @author     KUBO Atsuhiro <iteman2002@yahoo.co.jp>
+ * @copyright  2006 KUBO Atsuhiro <iteman2002@yahoo.co.jp>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    Release: @package_version@
+ * @link       http://iteman.typepad.jp/piece/
+ * @see        Piece_Unity_Plugin_Renderer_Flexy
+ * @since      Class available since Release 0.2.0
+ */
+class Piece_Unity_Plugin_Renderer_FlexyTestCase extends Piece_Unity_Plugin_Renderer_CompatibilityTest
+{
+
+    // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    var $_target = 'Flexy';
+
+    /**#@-*/
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    function _clear($view)
+    {
+        $files = array(dirname(__FILE__) . "/$view.html.en.php",
+                       dirname(__FILE__) . "/$view.html.gettext.serial"
+                       );
+        foreach ($files as $file) {
+            if (file_exists($file)) {
+                unlink($file);
+            }
+        }
+    }
+
+    function &_getConfig()
+    {
+        $config = &new Piece_Unity_Config();
+        $config->setConfiguration('Dispatcher_Simple', 'actionDirectory', dirname(__FILE__));
+        $config->setConfiguration('Renderer_Flexy', 'templateDir', dirname(__FILE__));
+        $config->setConfiguration('Renderer_Flexy', 'compileDir', dirname(__FILE__));
+        $config->setExtension('View', 'renderer', 'Renderer_Flexy');
+
+        return $config;
+    }
+
+    /**#@-*/
+
+    // }}}
+}
+
+// }}}
+
+function setBaz(&$foo)
+{
+    $foo->bar = 'baz';
+}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: iso-8859-1
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ */
+?>
