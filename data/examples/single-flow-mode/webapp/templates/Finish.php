872d4747ee74b6c484f249881cddf3140fbbb8e4
KUBO Atsuhiro <kubo@iteman.jp>
2006-07-01 14:55:50 +0000

    - Removed.

--- data/examples/single-flow-mode/webapp/templates/Finish.php
+++
@@ -1,6 +0,0 @@
-<html>
-  <body>
-    <h1>Registration finished!</h1>
-    <a href="index.php?<?php echo "{$__eventNameKey}=return" ?>">Restart registration flow.</a>
-  </body>
-</html>
--- data/examples/single-flow-mode/webapp/templates/Static/ActionAndDisplay.php
+++
@@ -1,5 +0,0 @@
-<html>
-  <body>
-    <h1><?php echo $dynamicContent ?></h1>
-  </body>
-</html>
