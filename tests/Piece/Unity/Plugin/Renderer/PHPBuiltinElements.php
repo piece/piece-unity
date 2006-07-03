04a6dde3ff1083b5d56c1f563f73c17197e92d14
KUBO Atsuhiro <kubo@iteman.jp>
2006-07-03 01:50:51 +0000

    - Updated for '__baseURLPath'.

--- tests/Piece/Unity/Plugin/Renderer/PHPBuiltinElements.php
+++ tests/Piece/Unity/Plugin/Renderer/PHPBuiltinElements.php
@@ -2,6 +2,7 @@
 if (isset($__request) && is_a($__request, 'Piece_Unity_Request')
     && isset($__session) && is_a($__session, 'Piece_Unity_Session')
     && isset($__eventNameKey) && is_string($__eventNameKey)
+    && isset($__baseURLPath) && is_string($__baseURLPath)
     ) {
     print 'OK';
 }
