dea76b8aee1807d0d41eebef06bcc447c2df2f23
KUBO Atsuhiro <kubo@iteman.jp>
2006-07-04 03:38:47 +0000

    - Fixed an invalid argument.

--- Piece/Unity/Plugin/Renderer/Flexy.php
+++ Piece/Unity/Plugin/Renderer/Flexy.php
@@ -139,7 +139,7 @@ class Piece_Unity_Plugin_Renderer_Flexy extends Piece_Unity_Plugin_Common
         $resultOfOutputObject = $flexy->outputObject($controller, $formElements);
         if (PEAR::isError($resultOfOutputObject)) {
             Piece_Unity_Error::pushCallback(create_function('$error', 'return ' . PEAR_ERRORSTACK_PUSHANDLOG . ';'));
-            Piece_Unity_Error::pushPearError($resultOfCompile,
+            Piece_Unity_Error::pushPearError($resultOfOutputObject,
                                              PIECE_UNITY_ERROR_INVOCATION_FAILED,
                                              'Failed to invoke the plugin [ ' . __CLASS__ . ' ].',
                                              'warning',
