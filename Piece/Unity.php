9206d0851d7fefb2c28c61c025dcc0d9e2bbcd4c
KUBO Atsuhiro <kubo@iteman.jp>
2006-07-09 11:54:25 +0000

    - Changed my email address.

--- Piece/Unity.php
+++ Piece/Unity.php
@@ -4,7 +4,7 @@
 /**
  * PHP versions 4 and 5
  *
- * Copyright (c) 2006, KUBO Atsuhiro <iteman2002@yahoo.co.jp>
+ * Copyright (c) 2006, KUBO Atsuhiro <iteman@users.sourceforge.net>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,8 +29,8 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  * @package    Piece_Unity
- * @author     KUBO Atsuhiro <iteman2002@yahoo.co.jp>
- * @copyright  2006 KUBO Atsuhiro <iteman2002@yahoo.co.jp>
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
  * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
  * @version    SVN: $Id$
  * @link       http://iteman.typepad.jp/piece/
@@ -53,8 +53,8 @@ $GLOBALS['PIECE_UNITY_Root_Plugin'] = 'Root';
  * A single entry point for Piece_Unity applications.
  *
  * @package    Piece_Unity
- * @author     KUBO Atsuhiro <iteman2002@yahoo.co.jp>
- * @copyright  2006 KUBO Atsuhiro <iteman2002@yahoo.co.jp>
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
  * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
  * @version    Release: @package_version@
  * @since      Class available since Release 0.1.0
