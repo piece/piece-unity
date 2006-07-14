f57b8a3b5922262e5ae6fdf3b0037ae2cd0e28ec
KUBO Atsuhiro <kubo@iteman.jp>
2006-07-14 02:46:31 +0000

    - Added.

---
+++ Piece/Unity/Plugin/Interceptor/ProxyBasePath.php
@@ -0,0 +1,143 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP versions 4 and 5
+ *
+ * Copyright (c) 2006, KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Piece_Unity
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    SVN: $Id$
+ * @link       http://iteman.typepad.jp/piece/
+ * @since      File available since Release 0.5.0
+ */
+
+require_once 'Piece/Unity/Plugin/Common.php';
+
+// {{{ Piece_Unity_Plugin_Interceptor_ProxyBasePath
+
+/**
+ * An interceptor to adjust the script name and the base path of the current
+ * request which are held in the Piece_Unity_Context object.
+ * This interceptor is used when your web servers are used as reverse proxies.
+ *
+ * The script name and the base path are both relative paths since they based
+ * on SCRIPT_NAME environment variable. The following is a example of a
+ * context change when 'path' configuration point is set to '/foo'.
+ *
+ * <pre>
+ * 'path' Configuration Point: /foo
+ *
+ * Requested URL (frontend): http://example.org/foo/bar/baz.php
+ * Requested URL (backend):  http://backend.example.org/bar/baz.php
+ * Script Name (original):   /bar/baz.php
+ * Script Name (original):   /bar
+ * Script Name (adjusted):   /foo/bar/baz.php
+ * Script Name (adjusted):   /foo/bar
+ * </pre>
+ *
+ * @package    Piece_Unity
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    Release: @package_version@
+ * @link       http://iteman.typepad.jp/piece/
+ * @since      Class available since Release 0.5.0
+ */
+class Piece_Unity_Plugin_Interceptor_ProxyBasePath extends Piece_Unity_Plugin_Common
+{
+
+    // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access public
+     */
+
+    // }}}
+    // {{{ constructor
+
+    /**
+     * Defines extension points and configuration points for the plugin.
+     */
+    function Piece_Unity_Plugin_Interceptor_ProxyBasePath()
+    {
+        parent::Piece_Unity_Plugin_Common();
+        $this->_addConfigurationPoint('path', null);
+    }
+
+    // }}}
+    // {{{ invoke()
+
+    /**
+     * Invokes the plugin specific code.
+     */
+    function invoke()
+    {
+        $path = $this->getConfiguration('path');
+        if (!is_null($path)) {
+            $this->_context->setBasePath($path . $this->_context->getBasePath());
+            $this->_context->setScriptName($path . $this->_context->getScriptName());
+        }
+    }
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+ 
+    /**#@-*/
+
+    // }}}
+}
+
+// }}}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: iso-8859-1
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ */
+?>
