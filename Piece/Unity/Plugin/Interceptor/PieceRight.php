8e1e3cd98ac67455eab91fcd576af8117d13dd5c
KUBO Atsuhiro <kubo@iteman.jp>
2006-07-20 15:36:43 +0000

    - Added.

---
+++ Piece/Unity/Plugin/Interceptor/PieceRight.php
@@ -0,0 +1,150 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP versions 4 and 5
+ *
+ * Copyright (c) 2006, KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Piece_Unity
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    SVN: $Id$
+ * @link       http://iteman.typepad.jp/piece/
+ * @since      File available since Release 0.6.0
+ */
+
+require_once 'Piece/Unity/Plugin/Common.php';
+require_once 'Piece/Right.php';
+require_once 'Piece/Right/Validator/Factory.php';
+require_once 'Piece/Unity/Error.php';
+
+// {{{ Piece_Unity_Plugin_Interceptor_PieceRight
+
+/**
+ * An interceptor to set a Piece_Right object to the current application
+ * context.
+ *
+ * @package    Piece_Unity
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    Release: @package_version@
+ * @link       http://iteman.typepad.jp/piece/
+ * @since      Class available since Release 0.6.0
+ */
+class Piece_Unity_Plugin_Interceptor_PieceRight extends Piece_Unity_Plugin_Common
+{
+
+    // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access public
+     */
+
+    // }}}
+    // {{{ constructor
+
+    /**
+     * Defines extension points and configuration points for the plugin.
+     */
+    function Piece_Unity_Plugin_Interceptor_PieceRight()
+    {
+        parent::Piece_Unity_Plugin_Common();
+        $this->_addConfigurationPoint('configDirectory', null);
+        $this->_addConfigurationPoint('cacheDirectory', null);
+        $this->_addConfigurationPoint('validatorDirectories', array());
+    }
+
+    // }}}
+    // {{{ invoke()
+
+    /**
+     * Invokes the plugin specific code.
+     *
+     * @return boolean
+     */
+    function invoke()
+    {
+        $validatorDirectories = $this->getConfiguration('validatorDirectories');
+        if (is_array($validatorDirectories)) {
+            foreach (array_reverse($validatorDirectories) as $validatorDirectory) {
+                Piece_Right_Validator_Factory::addValidatorDirectory($validatorDirectory);
+            }
+        } else {
+            Piece_Unity_Error::pushCallback(create_function('$error', 'return ' . PEAR_ERRORSTACK_PUSHANDLOG . ';'));
+            Piece_Unity_Error::push(PIECE_UNITY_ERROR_INVALID_CONFIGURATION,
+                                    'Failed to configure the configuration point [ validatorDirectories ] at the plugin [ ' . __CLASS__ . ' ].',
+                                    'warning',
+                                    array('plugin' => __CLASS__)
+                                    );
+            Piece_Unity_Error::popCallback();
+        }
+
+        $right = &new Piece_Right($this->getConfiguration('configDirectory'),
+                                  $this->getConfiguration('cacheDirectory')
+                                  );
+        $this->_context->setAttributeByRef('_pieceRight', $right);
+
+        return true;
+    }
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    // }}}
+}
+
+// }}}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: iso-8859-1
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ */
+?>
---
+++ tests/Piece/Unity/Plugin/Interceptor/PieceRightTestCase.php
@@ -0,0 +1,157 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP versions 4 and 5
+ *
+ * Copyright (c) 2006, KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Piece_Unity
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    SVN: $Id$
+ * @link       http://iteman.typepad.jp/piece/
+ * @see        Piece_Unity_Plugin_Interceptor_PieceRight
+ * @since      File available since Release 0.6.0
+ */
+
+require_once 'PHPUnit.php';
+require_once 'Piece/Unity/Plugin/Interceptor/PieceRight.php';
+require_once 'Piece/Unity/Context.php';
+require_once 'Piece/Unity/Config.php';
+require_once 'Piece/Right/Config.php';
+require_once 'Cache/Lite/File.php';
+
+// {{{ Piece_Unity_Plugin_Interceptor_PieceRightTestCase
+
+/**
+ * TestCase for Piece_Unity_Plugin_Interceptor_PieceRight
+ *
+ * @package    Piece_Unity
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    Release: @package_version@
+ * @link       http://iteman.typepad.jp/piece/
+ * @see        Piece_Unity_Plugin_Interceptor_PieceRight
+ * @since      Class available since Release 0.6.0
+ */
+class Piece_Unity_Plugin_Interceptor_PieceRightTestCase extends PHPUnit_TestCase
+{
+
+    // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access public
+     */
+
+    function tearDown()
+    {
+        $cache = &new Cache_Lite_File(array('cacheDir' => dirname(__FILE__) . '/',
+                                            'masterFile' => '',
+                                            'automaticSerialization' => true,
+                                            'errorHandlingAPIBreak' => true)
+                                      );
+        $cache->clean();
+        $context = &Piece_Unity_Context::singleton();
+        $context->clear();
+    }
+
+    function testValidation()
+    {
+        $oldValidatorDirectories = $GLOBALS['PIECE_RIGHT_Validator_Directories'];
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        $_POST['first_name'] = 'Foo';
+        $_POST['last_name'] = 'Bar';
+        $_POST['phone'] = '0123456789';
+        $_POST['country'] = 'Japan';
+        $_POST['hello'] = 'Hello World';
+        $dynamicConfig = &new Piece_Right_Config();
+        $dynamicConfig->addValidation('phone', 'Required');
+        $dynamicConfig->addValidation('phone', 'Length', array('min' => 10, 'max' => 11));
+
+        $config = &new Piece_Unity_Config();
+        $config->setConfiguration('Interceptor_PieceRight', 'configDirectory', dirname(__FILE__));
+        $config->setConfiguration('Interceptor_PieceRight', 'cacheDirectory', dirname(__FILE__));
+        $config->setConfiguration('Interceptor_PieceRight', 'validatorDirectories', array(dirname(__FILE__)));
+        $context = &Piece_Unity_Context::singleton();
+        $context->setConfiguration($config);
+
+        $interceptor = &new Piece_Unity_Plugin_Interceptor_PieceRight();
+        $interceptor->invoke();
+
+        $right = &$context->getAttribute('_pieceRight');
+
+        $this->assertTrue(is_a($right, 'Piece_Right'));
+        $this->assertTrue($right->validate('PieceRightExample', $dynamicConfig));
+
+        unset($_POST['hello']);
+        unset($_POST['country']);
+        unset($_POST['phone']);
+        unset($_POST['last_name']);
+        unset($_POST['first_name']);
+        unset($_SERVER['REQUEST_METHOD']);
+
+        $GLOBALS['PIECE_RIGHT_Validator_Instances'] = array();
+        $GLOBALS['PIECE_RIGHT_Validator_Directories'] = $oldValidatorDirectories;
+    }
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    // }}}
+}
+
+// }}}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: iso-8859-1
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ */
+?>
