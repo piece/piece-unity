f30a8b6193792c1ee553312b0ff7557541a8b38f
KUBO Atsuhiro <kubo@iteman.jp>
2006-12-07 03:38:27 +0000

    - Added.

---
+++ Piece/Unity/Session/Preload.php
@@ -0,0 +1,143 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP versions 4 and 5
+ *
+ * Copyright (c) 2006 KUBO Atsuhiro <iteman@users.sourceforge.net>,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Piece_Unity
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    SVN: $Id$
+ * @link       http://piece-framework.com/piece-unity/
+ * @since      File available since Release 0.9.0
+ */
+
+// {{{ Piece_Unity_Session_Preload
+
+/**
+ * A class *pre*loader for restoring objects in session.
+ *
+ * @package    Piece_Unity
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    Release: @package_version@
+ * @link       http://piece-framework.com/piece-unity/
+ * @since      Class available since Release 0.9.0
+ */
+class Piece_Unity_Session_Preload
+{
+
+    // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    var $_services = array();
+
+    /**#@-*/
+
+    /**#@+
+     * @access public
+     */
+
+    // }}}
+    // {{{ __wakeup
+
+    /**
+     * Preloads classes by service specific callback.
+     */
+    function __wakeup()
+    {
+        foreach (array_keys($this->_services) as $service) {
+            foreach ($this->_services[$service]['classes'] as $class => $payload) {
+                call_user_func($this->_services[$service]['callback'], $class, $payload);
+            }
+        }
+    }
+
+    // }}}
+    // {{{ addClass()
+
+    /**
+     * Adds a class for preload to the given service.
+     *
+     * @param string $service
+     * @param string $class
+     * @param mixed  $payload
+     */
+    function addClass($service, $class, $payload = null)
+    {
+        $this->_services[$service]['classes'][$class] = $payload;
+    }
+
+    // }}}
+    // {{{ setCallback()
+
+    /**
+     * Sets a callback to the given service.
+     *
+     * @param string   $service
+     * @param callback $callback
+     */
+    function setCallback($service, $callback)
+    {
+        $this->_services[$service]['callback'] = $callback;
+    }
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    // }}}
+}
+
+// }}}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: iso-8859-1
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ */
+?>
