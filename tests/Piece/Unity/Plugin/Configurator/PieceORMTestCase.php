d4130484a4d186f8672ff444bc870be72121b95f
KUBO Atsuhiro <kubo@iteman.jp>
2007-05-21 02:00:41 +0000

    - Added. (Ticket #48)

---
+++ tests/Piece/Unity/Plugin/Configurator/PieceORMTestCase.php
@@ -0,0 +1,168 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP versions 4 and 5
+ *
+ * Copyright (c) 2006-2007 KUBO Atsuhiro <iteman@users.sourceforge.net>,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Piece_Unity
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006-2007 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    SVN: $Id$
+ * @link       http://piece-framework.com/piece-unity/
+ * @see        Piece_Unity_Plugin_Configurator_PieceORM
+ * @since      File available since Release 0.12.0
+ */
+
+require_once 'PHPUnit.php';
+require_once 'Piece/Unity/Plugin/Configurator/PieceORM.php';
+require_once 'Piece/ORM/Context.php';
+require_once 'Cache/Lite.php';
+require_once 'Piece/Unity/Config.php';
+require_once 'Piece/ORM/Metadata/Factory.php';
+require_once 'Piece/ORM/Mapper/Factory.php';
+require_once 'Piece/ORM/Context.php';
+require_once 'Piece/ORM/Error.php';
+require_once 'Piece/Unity/Context.php';
+
+if (version_compare(phpversion(), '5.0.0', '<')) {
+    require_once 'spyc.php';
+} else {
+    require_once 'spyc.php5';
+}
+
+// {{{ Piece_Unity_Plugin_Configurator_PieceORMTestCase
+
+/**
+ * TestCase for Piece_Unity_Plugin_Configurator_PieceORM
+ *
+ * @package    Piece_Unity
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006-2007 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    Release: @package_version@
+ * @link       http://piece-framework.com/piece-unity/
+ * @see        Piece_Unity_Plugin_Configurator_ORM
+ * @since      Class available since Release 0.12.0
+ */
+class Piece_Unity_Plugin_Configurator_PieceORMTestCase extends PHPUnit_TestCase
+{
+
+    // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    var $_cacheDirectory;
+
+    /**#@-*/
+
+    /**#@+
+     * @access public
+     */
+
+    function setUp()
+    {
+        $this->_cacheDirectory = dirname(__FILE__) . '/' . basename(__FILE__, '.php');
+    }
+
+    function tearDown()
+    {
+        $GLOBALS['PIECE_ORM_Configured'] = false;
+        Piece_ORM_Metadata_Factory::clearInstances();
+        Piece_ORM_Mapper_Factory::clearInstances();
+        Piece_ORM_Context::clear();
+        $cache = &new Cache_Lite(array('cacheDir' => "{$this->_cacheDirectory}/",
+                                       'automaticSerialization' => true,
+                                       'errorHandlingAPIBreak' => true)
+                                 );
+        $cache->clean();
+        Piece_ORM_Error::clearErrors();
+        Piece_ORM_Error::popCallback();
+        Piece_Unity_Context::clear();
+    }
+
+    function testConfigure()
+    {
+        $config = &new Piece_Unity_Config();
+        $config->setConfiguration('Configurator_PieceORM', 'configDirectory', $this->_cacheDirectory);
+        $config->setConfiguration('Configurator_PieceORM', 'cacheDirectory', $this->_cacheDirectory);
+        $config->setConfiguration('Configurator_PieceORM', 'mapperConfigDirectory', $this->_cacheDirectory);
+        $context = &Piece_Unity_Context::singleton();
+        $context->setConfiguration($config);
+
+        $configurator = &new Piece_Unity_Plugin_Configurator_PieceORM();
+        $configurator->invoke();
+
+        $yaml = Spyc::YAMLLoad("{$this->_cacheDirectory}/piece-orm-config.yaml");
+
+        $this->assertTrue(count($yaml));
+
+        $context = &Piece_ORM_Context::singleton();
+        $config = &$context->getConfiguration();
+
+        foreach ($yaml as $configuration) {
+            $this->assertEquals($configuration['dsn'], $config->getDSN($configuration['name']));
+            $this->assertEquals($configuration['options'], $config->getOptions($configuration['name']));
+        }
+
+        $this->assertEquals($this->_cacheDirectory, Piece_ORM_Mapper_Factory::setConfigDirectory('./foo'));
+        $this->assertEquals($this->_cacheDirectory, Piece_ORM_Mapper_Factory::setCacheDirectory('./bar'));
+        $this->assertEquals($this->_cacheDirectory, Piece_ORM_Metadata_Factory::setCacheDirectory('./baz'));
+    }
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    // }}}
+}
+
+// }}}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: iso-8859-1
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ */
+?>
