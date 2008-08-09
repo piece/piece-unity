f6f096b93f017f3083dec2f072cc27188c9fd5e1
KUBO Atsuhiro <kubo@iteman.jp>
2008-08-09 13:56:04 +0000

    - Refactored.

--- Piece/Unity/HTTPStatus.php
+++ Piece/Unity/HTTPStatus.php
@@ -67,47 +67,48 @@ class Piece_Unity_HTTPStatus
      */
 
     var $_statusCode;
-    var $_reasonPharses = array(100 => 'Continue',
-                                101 => 'Switching Protocols',
-                                200 => 'OK',
-                                201 => 'Created',
-                                202 => 'Accepted',
-                                203 => 'Non-Authoritative Information',
-                                204 => 'No Content',
-                                205 => 'Reset Content',
-                                206 => 'Partial Content',
-                                300 => 'Multiple Choices',
-                                301 => 'Moved Permanently',
-                                302 => 'Found',
-                                303 => 'See Other',
-                                304 => 'Not Modified',
-                                305 => 'Use Proxy',
-                                307 => 'Temporary Redirect',
-                                400 => 'Bad Request',
-                                401 => 'Unauthorized',
-                                402 => 'Payment Required',
-                                403 => 'Forbidden',
-                                404 => 'Not Found',
-                                405 => 'Method Not Allowed',
-                                406 => 'Not Acceptable',
-                                407 => 'Proxy Authentication Required',
-                                408 => 'Request Time-out',
-                                409 => 'Conflict',
-                                410 => 'Gone',
-                                411 => 'Length Required',
-                                412 => 'Precondition Failed',
-                                413 => 'Request Entity Too Large',
-                                414 => 'Request-URI Too Large',
-                                415 => 'Unsupported Media Type',
-                                416 => 'Requested range not satisfiable',
-                                417 => 'Expectation Failed',
-                                500 => 'Internal Server Error',
-                                501 => 'Not Implemented',
-                                502 => 'Bad Gateway',
-                                503 => 'Service Unavailable',
-                                504 => 'Gateway Time-out',
-                                505 => 'HTTP Version not supported'
-                                );
+    var $_statusCodes = array(100 => 'Continue',
+                              101 => 'Switching Protocols',
+                              200 => 'OK',
+                              201 => 'Created',
+                              202 => 'Accepted',
+                              203 => 'Non-Authoritative Information',
+                              204 => 'No Content',
+                              205 => 'Reset Content',
+                              206 => 'Partial Content',
+                              300 => 'Multiple Choices',
+                              301 => 'Moved Permanently',
+                              302 => 'Found',
+                              303 => 'See Other',
+                              304 => 'Not Modified',
+                              305 => 'Use Proxy',
+                              307 => 'Temporary Redirect',
+                              400 => 'Bad Request',
+                              401 => 'Unauthorized',
+                              402 => 'Payment Required',
+                              403 => 'Forbidden',
+                              404 => 'Not Found',
+                              405 => 'Method Not Allowed',
+                              406 => 'Not Acceptable',
+                              407 => 'Proxy Authentication Required',
+                              408 => 'Request Time-out',
+                              409 => 'Conflict',
+                              410 => 'Gone',
+                              411 => 'Length Required',
+                              412 => 'Precondition Failed',
+                              413 => 'Request Entity Too Large',
+                              414 => 'Request-URI Too Large',
+                              415 => 'Unsupported Media Type',
+                              416 => 'Requested range not satisfiable',
+                              417 => 'Expectation Failed',
+                              500 => 'Internal Server Error',
+                              501 => 'Not Implemented',
+                              502 => 'Bad Gateway',
+                              503 => 'Service Unavailable',
+                              504 => 'Gateway Time-out',
+                              505 => 'HTTP Version not supported'
+                              );
+    var $_sentStatusLine;
 
     /**#@-*/
 
@@ -122,10 +123,11 @@ class Piece_Unity_HTTPStatus
      * Sets a status code.
      *
      * @param integer $statusCode
+     * @throws PIECE_UNITY_ERROR_NOT_FOUND
      */
     function Piece_Unity_HTTPStatus($statusCode)
     {
-        if (!array_key_exists($statusCode, $this->_reasonPharses)) {
+        if (!array_key_exists($statusCode, $this->_statusCodes)) {
             Piece_Unity_Error::push(PIECE_UNITY_ERROR_NOT_FOUND,
                                     "Unknown status code [ $statusCode ], be sure the status code is correct."
                                     );
@@ -146,7 +148,7 @@ class Piece_Unity_HTTPStatus
         $statusLine = sprintf('%s %d %s',
                               $_SERVER['SERVER_PROTOCOL'],
                               $this->_statusCode,
-                              $this->_reasonPharses[ $this->_statusCode ]
+                              $this->_statusCodes[ $this->_statusCode ]
                               );
         @header($statusLine);
 
