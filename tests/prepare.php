99390060be5a061650f172afeb9df34d7e29c142
KUBO Atsuhiro <kubo@iteman.jp>
2009-04-10 16:31:34 +0900

    - Improved the header comment.

--- tests/prepare.php
+++ tests/prepare.php
@@ -30,8 +30,8 @@
  *
  * @package    Piece_Unity
  * @copyright  2007-2009 KUBO Atsuhiro <kubo@iteman.jp>
- * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
- * @version    GIT: $Id$
+ * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @version    Release: @package_version@
  * @since      File available since Release 1.0.0
  */
 
